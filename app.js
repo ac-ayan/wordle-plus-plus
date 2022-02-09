fc805807533f1ae404754a9b1b4fbb9382d92d94
Ayan Chakraborty <1905741@kiit.ac.in>
2022-02-09 15:03:33 +0530

    Adding link config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const messageSection = document.querySelector(".message-section");
 
 let wordle;
 const getWordle = () => {
-  fetch("http://localhost:8000/word")
+  fetch("https://wordle-plus-plus.herokuapp.com/word")
     .then((response) => response.json())
     .then((json) => {
       //console.log(json);
@@ -132,7 +132,7 @@ const checkRow = () => {
   //console.log("Inside Checkrow");
   const guess = guessRows[currentRow].join("");
   if (currentTile > 4) {
-    fetch(`http://localhost:8000/check/?word=${guess}`)
+    fetch(`https://wordle-plus-plus.herokuapp.com//check/?word=${guess}`)
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
